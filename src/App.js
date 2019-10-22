9bb0abdfce3beaabdf69810365b4dba6a8c720e9
Sergio Valencia <Sergio@Sergios-MacBook-Pro.local>
2019-10-22 16:46:35 +0200

    using BrowserRouter and Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route
+            path="/user/:username"
+            render={({ match }) => {
+              return <h1>Welcome {match.params.username}</h1>;
+            }}
+          />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
