e29cef554e556e8935171b98e0d0299ec1bc3409
Sergio Valencia <Sergio@Sergios-MacBook-Pro.local>
2019-10-22 17:19:31 +0200

    Halfway to login / logout functionality

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,28 +3,34 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink
-          exact
-          className="App-link"
-          to="/"
-          activeClassName="Active-link"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="App-link" to="about" activeClassName="Active-link">
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="App-link" to="user" activeClassName="Active-link">
-          User
-        </NavLink>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <NavLink
+            exact
+            className="App-link"
+            to="/"
+            activeClassName="Active-link"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="App-link"
+            to="about"
+            activeClassName="Active-link"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="App-link" to="user" activeClassName="Active-link">
+            User
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Navigation from "../components/navigation";
 
 function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function loginHandle() {
+    setLoggedIn(!loggedIn);
+  }
   return (
     <div>
       <Navigation />
       <h1>Home Page</h1>
+      <button onClick={loginHandle}>{loggedIn ? "Logout" : "Login"}</button>
     </div>
   );
 }
